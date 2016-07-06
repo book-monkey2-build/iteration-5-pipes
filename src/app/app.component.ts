0afef2eb0c914f2ae949e46ec1073a8ed91c0ef5
The Buildbot <buildbot@angular2buch.de>
2016-07-06 19:15:08 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/5ee019398072f80428576689011edd59b920161a
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/142846707

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
+  styles: [
+    `.router-link-active {
+      color: #ffffff !important;
+      background: #DB282C !important;
+    }`
+  ],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [HTTP_PROVIDERS]
+})
+export class AppComponent { }
