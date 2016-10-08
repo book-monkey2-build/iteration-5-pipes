a958b61e64a8f7cdeb5ae31900ca0c0b5b7fde7c
The Buildbot <buildbot@angular2buch.de>
2016-10-08 21:24:36 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/7768f55393165c4b34c2303bc0213500d1c4fc14
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/166109872

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookListItemComponent } from './book-list-item/book-list-item.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BookFormComponent } from './book-form/book-form.component';
+
+import { BookStoreService } from './shared/book-store.service';
+import { IsbnPipe } from './shared/isbn-pipe';
+
+import { AppRoutingModule } from './app-routing.module.one-app';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BookListComponent,
+    BookListItemComponent,
+    BookDetailsComponent,
+    BookFormComponent,
+    IsbnPipe
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+    BookStoreService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
